5b1351b87597510c0d011793c22aa971acdc262a
Bipo04 <ledang1324@gmail.com>
2024-11-23 22:28:21 +0700

    bai2

--- Lab 3/AimsProject/src/Aims.java
+++ Lab 3/AimsProject/src/Aims.java
@@ -1,32 +1,39 @@
 public class Aims {
-	public static void main(String[] args) {
-		Cart anOrder = new Cart();
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
 
-		DigitalVideoDisc dvd_1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-		anOrder.addDigitalVideoDisc(dvd_1);
+        DigitalVideoDisc dvd_1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd_1);
 
-		DigitalVideoDisc dvd_2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-		anOrder.addDigitalVideoDisc(dvd_2);
+        DigitalVideoDisc dvd_2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd_2);
 
-		DigitalVideoDisc dvd_3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-		anOrder.addDigitalVideoDisc(dvd_3);
+        DigitalVideoDisc dvd_3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd_3);
 
-		System.out.print("Total Cost is: ");
-		System.out.println(anOrder.TotalCost());
+        System.out.print("Total Cost is: ");
+        System.out.println(anOrder.TotalCost());
+        System.out.println("==========================================================");
 
-		System.out.println("==========================================================");
-		System.out.println("Test chức năng loại bỏ đĩa khỏi giỏ");
-		anOrder.RemoveDigitalVideoDisc(dvd_1);
-		anOrder.RemoveDigitalVideoDisc(dvd_2);
-		anOrder.RemoveDigitalVideoDisc(dvd_3);
+//lab 3 kiểm tra tính năng ghi đè với tham số là 1 mảng
+        DigitalVideoDisc[] newDisc = {dvd_1, dvd_2};
+        anOrder.addDigitalVideoDisc(dvd_1, dvd_2, dvd_3);
+        System.out.print("Total Cost is: ");
+        System.out.println(anOrder.TotalCost());
+        System.out.println("==========================================================");
 
-		System.out.println("==========================================================");
-		System.out.println("Test chức năng loại bỏ đĩa khỏi giỏ đang trống");
-		anOrder.RemoveDigitalVideoDisc(dvd_1);
+        System.out.println("Test the functionality of removing a disc from the cart");
+        anOrder.RemoveDigitalVideoDisc(dvd_1);
+        anOrder.RemoveDigitalVideoDisc(dvd_2);
+        anOrder.RemoveDigitalVideoDisc(dvd_3);
+        System.out.println("==========================================================");
 
-		System.out.println("==========================================================");
-		System.out.println("Test chức năng loại bỏ đĩa khi đĩa không có trong giỏ");
-		anOrder.addDigitalVideoDisc(dvd_1);
-		anOrder.RemoveDigitalVideoDisc(dvd_3);
-	}
+        System.out.println("Test the functionality of removing a disc from an empty cart");
+        anOrder.RemoveDigitalVideoDisc(dvd_1);
+        System.out.println("==========================================================");
+        
+        System.out.println("Test the functionality of removing a disc that is not in the cart");
+        anOrder.addDigitalVideoDisc(dvd_1);
+        anOrder.RemoveDigitalVideoDisc(dvd_3);
+    }
 }
--- Lab 3/AimsProject/src/Cart.java
+++ Lab 3/AimsProject/src/Cart.java
@@ -1,45 +1,84 @@
-
 public class Cart {
-	public static final int MAX_NUMBERS_ORDERED = 20;
-	private DigitalVideoDisc[] itemsOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
-	private int qtyOrdered = 0;
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc[] itemsOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0;
 
-	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
-		if (qtyOrdered < MAX_NUMBERS_ORDERED) {
-			itemsOrdered[qtyOrdered++] = disc;
-			System.out.println("Đĩa \"" + disc.getTitle() + "\" đã được thêm vào giỏ.");
-		} else {
-			System.out.println("Giỏ đã đầy.");
-		}
-	}
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered++] = disc;
+            System.out.println("The disc \"" + disc.getTitle() + "\" has been added to the cart.");
+        } else {
+            System.out.println("The cart is full.");
+        }
+    }
 
-	public void RemoveDigitalVideoDisc(DigitalVideoDisc disc) {
-		if (qtyOrdered == 0) {
-			System.out.println("Giỏ đang trống.");
-			return;
-		}
-		for (int i = 0; i < qtyOrdered && itemsOrdered[i] != null; i++) {
-			if (itemsOrdered[i].equals(disc)) {
-				for (int j = i; j < qtyOrdered - 1; j++) {
-					itemsOrdered[j] = itemsOrdered[j + 1];
-				}
-				itemsOrdered[qtyOrdered - 1] = null;
-				System.out.println("Đĩa \"" + disc.getTitle() + "\" đã được loại bỏ.");
-				qtyOrdered--;
-				return;
-			}
-		}
-		System.out.println("Đĩa \"" + disc.getTitle() + "\" không có trong giỏ. Không thể loại bỏ.");
-	}
+//2.1
+    // public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+    //     if (qtyOrdered + dvdList.length <= MAX_NUMBERS_ORDERED) {
+    //         for (int i = 0 ; i < dvdList.length ; i++) {
+    //             itemsOrdered[qtyOrdered] = dvdList[i];
+    //             qtyOrdered++;
+    //             System.out.println("The disc \"" + dvdList[i].getTitle() + "\" has been added to the cart.");
+    //         }
+    //     }
+    //     else {
+    //         System.out.println("Please reduce the number of discs.");
+    //     }
+    // }
 
-	public float TotalCost() {
-		if (qtyOrdered == 0) {
-			return 0;
-		}
-		float totalCost = 0;
-		for (int i = 0; i < qtyOrdered && itemsOrdered[i] != null; i++) {
-			totalCost += itemsOrdered[i].getCost();
-		}
-		return totalCost;
-	}
+    public void addDigitalVideoDisc(DigitalVideoDisc... dvdList) {
+        if (qtyOrdered + dvdList.length <= MAX_NUMBERS_ORDERED) {
+            for (DigitalVideoDisc dvd : dvdList) {
+                itemsOrdered[qtyOrdered] = dvd;
+                qtyOrdered++;
+                System.out.println("The disc \"" + dvd.getTitle() + "\" has been added to the cart.");
+            }
+        } else {
+            System.out.println("Please reduce the number of discs.");
+        }
+    }
+
+//2.2
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        if (qtyOrdered + 2 <= MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered] = dvd1;
+            qtyOrdered++;
+            itemsOrdered[qtyOrdered] = dvd2;
+            qtyOrdered++;
+            System.out.println("\"" + dvd1.getTitle() + "\" and \"" + dvd2.getTitle() + "\" has been added to the cart.");
+        }
+        else {
+            System.out.println("Please reduce the number of discs.");
+        }
+    }
+
+    public void RemoveDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered == 0) {
+            System.out.println("The cart is empty.");
+            return;
+        }
+        for (int i = 0; i < qtyOrdered && itemsOrdered[i] != null; i++) {
+            if (itemsOrdered[i].equals(disc)) {
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                itemsOrdered[qtyOrdered - 1] = null;
+                System.out.println("The disc \"" + disc.getTitle() + "\" has been removed.");
+                qtyOrdered--;
+                return;
+            }
+        }
+        System.out.println("The disc \"" + disc.getTitle() + "\" is not in the cart. Unable to remove.");
+    }
+
+    public float TotalCost() {
+        if (qtyOrdered == 0) {
+            return 0;
+        }
+        float totalCost = 0;
+        for (int i = 0; i < qtyOrdered && itemsOrdered[i] != null; i++) {
+            totalCost += itemsOrdered[i].getCost();
+        }
+        return totalCost;
+    }
 }
